d343096f61ec17c634d958cf35a09e9cb2d12786
ali_rashidi <amialii.rashidii@gmail.com>
2023-06-13 18:05:36 +0330

    update my website home

--- src/app/personal/personal/personal.component.ts
+++ src/app/personal/personal/personal.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AccordionsItem } from './../../base/base-interface';
+import { AccordionsItem, ISkillsItem, IProjectItem } from './../../base/base-interface';
 
 @Component({
   selector: 'app-personal',
@@ -11,15 +11,25 @@ export class PersonalComponent implements OnInit {
     title: 'Categories',
     open: true,
     content: [
-      { 
+      {
         title: 'About',
         link: '#about',
         icon: 'flag.png'
       },
-      { 
+      {
+        title: 'Skills',
+        link: '#skills',
+        icon: 'angular.png'
+      },
+      {
         title: 'Projects',
         link: '#projects',
         icon: 'code.png'
+      },
+      {
+        title: 'Coffee',
+        link: '#coffee',
+        icon: 'coffee.png'
       }
     ],
   },
@@ -64,6 +74,78 @@ export class PersonalComponent implements OnInit {
       },
     ],
   }]
+
+  skills: ISkillsItem[] = [
+    {
+      name: "Html",
+      icon: "html.png",
+      rating: 4.5,
+    },
+    {
+      name: "CSS/SCSS",
+      icon: "css.png",
+      rating: 4.5,
+    },
+    {
+      name: "JavaScript",
+      icon: "javascript.png",
+      rating: 4,
+    },
+    {
+      name: "TypeScript",
+      icon: "typescript.png",
+      rating: 3.5,
+    },
+    {
+      name: "Angular",
+      icon: "angular.png",
+      rating: 4,
+    },
+    {
+      name: "Responsive Web Design",
+      icon: "responsive.png",
+      rating: 5,
+    },
+    {
+      name: "Git",
+      icon: "git.png",
+      rating: 3,
+    },
+    {
+      name: "Linux",
+      icon: "linux.png",
+      rating: 3,
+    },
+    {
+      name: "Bootstrap",
+      icon: "bootstrap.png",
+      rating: 3,
+    }
+  ]
+
+  projects: IProjectItem[] = [
+    {
+      name: "BourseTalent",
+      detail: `BourseTalent is a job placement system in the field of the stock market, developed
+        in HyvaTech Company for the Iran Financial Center. One of the notable features of this system
+        is its resume builder.`,
+      link: "https://boursetalent.ifc.ir/",
+    },
+    {
+      name: "DadGam",
+      detail: `Dadgam is a judicial system where users can easily seek legal advice from attorneys
+        within the system regarding their legal cases. Additionally, it provides intelligent capabilities
+        for creating contracts and legal petitions. Developed in HyvaTech Company.`,
+      link: "https://daadgam.com/",
+    },
+    {
+      name: "OnlineMenu",
+      detail: `In this online menu, restaurant owners have full access to all menu items, allowing them
+        to edit or delete them as needed. Additionally, there is a feature that allows them to view daily
+        menu visits.`,
+      link: "https://sky-lounge.ir/",
+    }
+  ]
   ngOnInit() {
 
   }
